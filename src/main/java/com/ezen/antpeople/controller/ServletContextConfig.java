b812fa8f50ddffaecd2563bcb81e3d655d05ee22
jonghwan kwon <whdghks1048@naver.com>
2019-07-16 17:32:57 +0900

    출퇴근 기능 작성중

--- src/main/java/com/ezen/antpeople/controller/ServletContextConfig.java
+++ src/main/java/com/ezen/antpeople/controller/ServletContextConfig.java
@@ -3,11 +3,25 @@ package com.ezen.antpeople.controller;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
-public class ServletContextConfig {
+public class ServletContextConfig implements WebMvcConfigurer {
+	
+	// Servlet-context의 resource
+	public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+	        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	    }
+	/*
+	 * public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+	 * registry.addResourceHandler("/resources/**").addResourceLocations(
+	 * "/resources/"); }
+	 */
+	
 	
 	@Bean
 	public InternalResourceViewResolver viewResolver() {
@@ -16,5 +30,5 @@ public class ServletContextConfig {
 		viewResolver.setSuffix(".jsp");
 		return viewResolver;
 	}
-
+	
 }
