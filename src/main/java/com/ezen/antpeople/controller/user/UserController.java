00fc205b8f7ec96b87c1352629d63c940b5c10bb
lx9203 <lx9203@naver.com>
2019-08-02 16:41:11 +0900

    Update UserController.java

--- src/main/java/com/ezen/antpeople/controller/user/UserController.java
+++ src/main/java/com/ezen/antpeople/controller/user/UserController.java
@@ -47,21 +47,18 @@ public class UserController {
 	// 로그인시 아이디 비밀번호 존재여부 체크
 	@RequestMapping(value = "logincheck", method = RequestMethod.POST)
 	@ResponseBody
-	public String logincheck(@RequestBody UserLoginDTO userLogin, Model model, HttpSession session, HttpServletResponse response)
-			throws Exception {
+	public String logincheck(@RequestBody UserLoginDTO userLogin, Model model, HttpSession session,
+			HttpServletResponse response) throws Exception {
 		logger.info("체크 페이지");
 		UserDetailDTO user = userService.findByEmail(userLogin.getEmail());
-		if (userService.verifiedPassword(user,userLogin.getPassword())) {
+		if (userService.verifiedPassword(user, userLogin.getPassword())) {
 			logger.info("로그인 성공");
 			model.addAttribute("user", user);
-//			if ( user.isUseCookie() ){ // dto 클래스 안에 useCookie 항목에 폼에서 넘어온 쿠키사용 여부(true/false)
-//                // 쿠키 사용이 체크되어 있으면
-//                // 쿠키를 생성하고 현재 로그인되어 있을 때 생성되었던 세션의 id를 쿠키에 저장
-//                Cookie cookie = new Cookie("loginCookie", session.getId());
-//                // 쿠키를 찾을 경로를 컨텍스트 경로로 변경
-//                cookie.setPath("/");
-//                cookie.setMaxAge(60*60*24*7); // 유효시간 7일
-//                response.addCookie(cookie);
+			Cookie cookie = new Cookie(user.getEmail(), user.getEmail());
+			cookie.setMaxAge(60*60*24*7); // 유효기한 1주일
+			cookie.setPath("/");
+			response.addCookie(cookie);
+			logger.info("cookie : " + cookie.getName());
 //            }
 			return "../main/mainpage";
 		} else {
@@ -69,20 +66,26 @@ public class UserController {
 			return "redirection";
 		}
 	}
-	
+
 	// 로그인 실패시 redirection으로 이동
 	@RequestMapping("redirection")
 	public String goRedirection() {
 		return "redirection";
 	}
-	
-	//로그아웃
+
+	// 로그아웃
 	@RequestMapping("logout")
-	public ModelAndView logout(SessionStatus sessionStatus, ModelAndView mv) {
+	public ModelAndView logout(HttpServletResponse response, SessionStatus sessionStatus, ModelAndView mv) {
+		logger.info("쿠키 제거");
+		Cookie cookie = new Cookie("loginCookie", null);
+		cookie.setMaxAge(5);
+		response.addCookie(cookie);
+		logger.info("쿠키값 : "+cookie.getValue());
+		logger.info("쿠키남은시간 : "+cookie.getMaxAge());
 		logger.info("세션 제거");
 		sessionStatus.setComplete();
-        mv.setViewName("redirect:../main/mainpage");
-        return mv;
+		mv.setViewName("redirect:../main/mainpage");
+		return mv;
 	}
 
 	// 회원가입 페이지로 이동 - 완료
@@ -95,14 +98,14 @@ public class UserController {
 		model.addAttribute("storeList", stores);
 		return "register";
 	}
-	
-	  // 회원가입 
-	  @RequestMapping(value="registercheck", method= RequestMethod.POST)
-	  @ResponseBody 
-	  public String registerCheck(@RequestBody UserDetailDTO user, Model model) throws Exception{
-		  String msg = userService.userSignUp(user);
-		  model.addAttribute("message", msg);
-		  return "login";
-	  }
+
+	// 회원가입
+	@RequestMapping(value = "registercheck", method = RequestMethod.POST)
+	@ResponseBody
+	public String registerCheck(@RequestBody UserDetailDTO user, Model model) throws Exception {
+		String msg = userService.userSignUp(user);
+		model.addAttribute("message", msg);
+		return "login";
+	}
 
 }
