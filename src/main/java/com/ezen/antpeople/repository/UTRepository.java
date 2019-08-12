1fe0cc41a373b5adc526086f35ee08dc38da8e9e
EthanKwon <ethkwon94@gmail.com>
2019-08-12 17:57:45 +0900

    Day27: todoListCount

--- src/main/java/com/ezen/antpeople/repository/UTRepository.java
+++ src/main/java/com/ezen/antpeople/repository/UTRepository.java
@@ -12,6 +12,7 @@ import com.ezen.antpeople.entity.TodoRelation;
 @Repository
 public interface UTRepository extends JpaRepository<TodoRelation, Integer> {
 	List<TodoRelation> findByToUser_id(int user_id);
+	List<TodoRelation> findByToUser_idAndState(int user_id, boolean state);
 	TodoRelation findByTodo_idAndToUser_id(int id, int user_id);
 	
 	@Transactional
