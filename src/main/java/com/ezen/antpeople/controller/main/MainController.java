3609c231346d36a55555a2eca4598a1a38204dac
lx9203 <lx9203@naver.com>
2019-08-14 09:36:53 +0900

    Update MainController.java

--- src/main/java/com/ezen/antpeople/controller/main/MainController.java
+++ src/main/java/com/ezen/antpeople/controller/main/MainController.java
@@ -23,7 +23,6 @@ import org.springframework.web.servlet.ModelAndView;
 import com.ezen.antpeople.dto.board.BbsDetailDTO;
 import com.ezen.antpeople.dto.board.NoticeDetailDTO;
 import com.ezen.antpeople.dto.sche.ScheUserDTO;
-
 import com.ezen.antpeople.dto.sche.ScheUserListDTO;
 import com.ezen.antpeople.dto.todo.TodoDetailDTO;
 import com.ezen.antpeople.dto.user.RoleDTO;
@@ -73,8 +72,6 @@ public class MainController {
 		List<ScheUserDTO> todayStaffList = new ArrayList<ScheUserDTO>(); //오늘 근무하는 사람
 		UserDetailDTO userDto = (UserDetailDTO) session.getAttribute("user");
 		String now = String.valueOf(date);
-		ScheUserListDTO own = new ScheUserListDTO();
-
 		int count = 0;
 		int counttodo = 0;
 		int countApply = 0;
@@ -85,7 +82,7 @@ public class MainController {
 			staffRefuseApply = userService.applyScheduleCount(user.get().getUser_id(), 3); // 일정 신청이 거절된 목록 수
 			todoList = todoService.TodoListAll(user.get());
 			sche = monthplanService.monthPlanList(userDto);
-			own = scheService.findAllMonthAndUser(userDto, now);
+			ScheUserListDTO own = scheService.findAllMonthAndUser(userDto, now);
 			count = StringUtils.countOccurrencesOf(sche, "true");
 			counttodo = StringUtils.countOccurrencesOf(todoList, "false");
 			countApply = StringUtils.countOccurrencesOf(own.toString(), "state\":0,");
